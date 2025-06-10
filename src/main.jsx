077a5dcb795f8e8b4fbb9bf1be60b5a450d9a65c
Miguel19x <leohxone@gmail.com>
2025-06-10 17:49:53 -0400

    Update Translation main.jsx
    Render i18n.js

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
   </React.StrictMode>,
 );
